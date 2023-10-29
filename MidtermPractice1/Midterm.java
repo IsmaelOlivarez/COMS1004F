8bde872e378f9b2871b4a6ce4b389a2d46e7c8df
Ismael Olivarez <io2235@columbia.edu>
2023-10-28 22:12:04 -0400

    python experimentals

--- MidtermPractice1/Midterm.java
+++ MidtermPractice1/Midterm.java
@@ -6,8 +6,42 @@ public class Midterm {
     public static void main (String args[]){
         Scanner s = new Scanner(System.in);
 
-        while(){
-            
+        //First Challenge
+        while(true){
+            System.out.println("Enter an integer: ");
+            int num = s.nextInt();
+
+            if(num == 5 || num == 7 || num == 11){
+                System.out.println("That's my lucky number!");
+            }else if(num <= 0){
+                break;
+            }
+
+        }
+
+
+        //Second Challenge
+
+        
+        while(true){
+            System.out.println("Enter a non-negative integer");
+            int num = s.nextInt();
+            if(num >= 0){
+                System.out.println(num);
+            }else{
+                break;
+            }
+        }
+
+
+        //Third Challenge
+
+        System.out.println("How large is the Array?");
+        int size = s.nextInt();
+        int[] userArray = new int[size];
+        for(int i = 0; i < userArray.length; i++){
+            System.out.println("Enter int value for position " + i + " of the Array");
+            userArray[i] = s.nextInt();
         }
     }
     
